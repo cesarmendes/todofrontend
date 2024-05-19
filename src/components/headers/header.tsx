9f837b7ac146c00763ebf61d919ae92eff6fa204
Cesar Mendes da silva <cesar.mendes.silva@outlook.com>
2024-05-19 18:10:36 -0300

    Adicionando projeto de frontend

---
+++ src/components/headers/header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
+
+var Header = () => {
+    return (
+        <header>
+            <AppBar>
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        {/* <MenuIcon /> */}
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Todo List
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </header>
+    );
+};
+
+export default Header;
